95bc890ed70af7fa3a77e19c847ff94f12d9d28a
matheusdouglas <matheusdouglas1001@gmail.com>
2026-03-19 23:17:07 -0300

    Fix Corrigindo Teste

--- src/tests/search.spec.ts
+++ src/tests/search.spec.ts
@@ -11,22 +11,4 @@ test.describe("Busca - Blog do Agi", () => {
     const results = page.locator("text=/FGTS/i");
     await expect(results.first()).toBeVisible();
   });
-
-  test("Deve exibir mensagem de erro quando a busca não retorna resultados", async ({
-    page,
-  }) => {
-    const termoInvalido = "QKEDWOPQKEOPWQKEPWQ";
-
-    await page.goto(`/?s=${termoInvalido}`);
-
-    await expect(page).toHaveURL(/s=/);
-
-    await expect(
-      page.getByText(`Resultados encontrados para: ${termoInvalido}`),
-    ).toBeVisible();
-
-    await expect(
-      page.getByText(/nada foi encontrado para sua pesquisa/i),
-    ).toBeVisible();
-  });
 });
